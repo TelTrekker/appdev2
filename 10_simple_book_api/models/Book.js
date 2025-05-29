3e636a953aeee7cba657b32926f15ab45d59d0c4
KM-sudo3 <168622429+TelTrekker@users.noreply.github.com>
2025-05-29 20:44:22 +0800

    Delete 10_simple_book_api directory

--- 10_simple_book_api/models/Book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: [true, 'Please enter book title'] },
-  author: { type: String, required: true, trim: [true, 'Please enter book title'] }
-}, { timestamps: true }
-);
-
-module.exports = mongoose.model('Book', bookSchema);
