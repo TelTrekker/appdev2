fd8a6cb5c0111d666d879ea4ec74b9227014e740
KM-sudo3 <168622429+TelTrekker@users.noreply.github.com>
2025-06-15 22:38:05 +0800

    Delete 11_simple_book_api_with_email_notification/10_simple_book_api_with_authentication directory

--- 11_simple_book_api_with_email_notification/10_simple_book_api_with_authentication/middleware/formparser.js
+++
@@ -1,7 +0,0 @@
-//so I can build body form
-const multer = require('multer');
-
-// No file uploads, so use memory storage
-const upload = multer();
-
-module.exports = upload.none(); // Accept only text fields (no files)
